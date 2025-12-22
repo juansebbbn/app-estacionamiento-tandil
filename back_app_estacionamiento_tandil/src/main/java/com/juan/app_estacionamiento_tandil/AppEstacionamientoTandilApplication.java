39427c77ae4a4622ab92b27ebca88d5693fd7cce
juansebbbn <juansebastianbadone@hotmail.com>
2025-12-22 14:56:35 -0300

    primer commit de prueba

---
+++ back_app_estacionamiento_tandil/src/main/java/com/juan/app_estacionamiento_tandil/AppEstacionamientoTandilApplication.java
@@ -0,0 +1,13 @@
+package com.juan.app_estacionamiento_tandil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEstacionamientoTandilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEstacionamientoTandilApplication.class, args);
+    }
+
+}
---
+++ back_app_estacionamiento_tandil/src/test/java/com/juan/app_estacionamiento_tandil/AppEstacionamientoTandilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.app_estacionamiento_tandil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEstacionamientoTandilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
