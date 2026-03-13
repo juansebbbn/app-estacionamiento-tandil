2daf22df5b4ecffdade6bdb4dc7d7b2a80684609
Juan Badone <juansb2001@hotmail.com>
2026-03-13 11:25:19 -0300

    fix: changing config for ci

--- back_app_estacionamiento_tandil/src/test/java/com/juan/app_estacionamiento_tandil/AppEstacionamientoTandilApplicationTests.java
+++ back_app_estacionamiento_tandil/src/test/java/com/juan/app_estacionamiento_tandil/AppEstacionamientoTandilApplicationTests.java
@@ -2,8 +2,10 @@ package com.juan.app_estacionamiento_tandil;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AppEstacionamientoTandilApplicationTests {
 
     @Test
